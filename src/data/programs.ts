333f26adf48047b9ae5e11482234ebbee1480445
[dyad] <git@dyad.sh>
2025-08-28 17:35:54 +0200

    [dyad] Aggiornato il titolo del programma "Premio Per Sempre" in "Premio Per Sempre con Diego". - wrote 1 file(s)

---
+++ src/data/programs.ts
@@ -0,0 +1,46 @@
+import { Program } from '../types/program';
+
+export const programs: Program[] = [
+  {
+    id: '1',
+    title: 'Premio Per Sempre con Diego',
+    description: 'Un viaggio emozionante attraverso le storie di persone comuni che hanno lasciato un segno indelebile.',
+    imageUrl: 'https://www.raiplay.it/resizegd/320x180/dl/img/2024/05/23/1716409000000_16x9_1716409000000-premio_per_sempre.jpg',
+    videoUrl: 'https://www.raiplay.it/video/2024/05/Premio-per-sempre---Puntata-del-23052024-e0211212-2211-4211-8211-221121221122.html',
+  },
+  {
+    id: '2',
+    title: 'Doc - Nelle tue mani',
+    description: 'Il dottor Andrea Fanti deve ricostruire la sua vita dopo un incidente che gli ha cancellato dodici anni di memoria.',
+    imageUrl: 'https://www.raiplay.it/resizegd/320x180/dl/img/2024/05/23/1716409000000_16x9_1716409000000-doc_nelle_tue_mani.jpg',
+    videoUrl: 'https://www.raiplay.it/video/2024/05/Doc---Nelle-tue-mani---Puntata-del-23052024-e0211212-2211-4211-8211-221121221122.html',
+  },
+  {
+    id: '3',
+    title: 'Il Paradiso delle Signore',
+    description: 'Le vicende del grande magazzino milanese e le vite delle persone che ci lavorano e lo frequentano.',
+    imageUrl: 'https://www.raiplay.it/resizegd/320x180/dl/img/2024/05/23/1716409000000_16x9_1716409000000-il_paradiso_delle_signore.jpg',
+    videoUrl: 'https://www.raiplay.it/video/2024/05/Il-Paradiso-delle-Signore---Puntata-del-23052024-e0211212-2211-4211-8211-221121221122.html',
+  },
+  {
+    id: '4',
+    title: 'Un Posto al Sole',
+    description: 'Le storie degli abitanti di Palazzo Palladini a Napoli, tra amori, intrighi e colpi di scena.',
+    imageUrl: 'https://www.raiplay.it/resizegd/320x180/dl/img/2024/05/23/1716409000000_16x9_1716409000000-un_posto_al_sole.jpg',
+    videoUrl: 'https://www.raiplay.it/video/2024/05/Un-Posto-al-Sole---Puntata-del-23052024-e0211212-2211-4211-8211-221121221122.html',
+  },
+  {
+    id: '5',
+    title: 'Che Dio ci aiuti',
+    description: 'Suor Angela e le sue consorelle aiutano chi è in difficoltà, tra misteri e risate.',
+    imageUrl: 'https://www.raiplay.it/resizegd/320x180/dl/img/2024/05/23/1716409000000_16x9_1716409000000-che_dio_ci_aiuti.jpg',
+    videoUrl: 'https://www.raiplay.it/video/2024/05/Che-Dio-ci-aiuti---Puntata-del-23052024-e0211212-2211-4211-8211-221121221122.html',
+  },
+  {
+    id: '6',
+    title: 'Don Matteo',
+    description: 'Don Matteo, un parroco con un talento speciale per risolvere i crimini, aiuta i Carabinieri di Spoleto.',
+    imageUrl: 'https://www.raiplay.it/resizegd/320x180/dl/img/2024/05/23/1716409000000_16x9_1716409000000-don_matteo.jpg',
+    videoUrl: 'https://www.raiplay.it/video/2024/05/Don-Matteo---Puntata-del-23052024-e0211212-2211-4211-8211-221121221122.html',
+  },
+];
