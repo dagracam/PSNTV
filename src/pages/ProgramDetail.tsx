df59757bc01efbeae2966fa9983fab63a8c5f58e
[dyad] <git@dyad.sh>
2025-08-28 19:56:51 +0200

    Reverted all changes back to version 055b0002cb52be69790299fc93dc280454121164

--- src/pages/ProgramDetail.tsx
+++ src/pages/ProgramDetail.tsx
@@ -31,7 +31,7 @@ const ProgramDetail: React.FC = () => {
           <iframe
             width="100%"
             height="545"
-            src={program.videoUrl} {/* Ora usa il videoUrl dinamico del programma */}
+            src="https://web.psntv.eu/embed-playlist/persemprenews/Premio_per_Sempre_Scugnizzi"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
