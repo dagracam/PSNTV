f6ad6a97873cc5756b2e7b6e7e5e7d8e78f54f4e
[dyad] <dagracaman@gmail.com>
2025-09-03 00:12:24 +0200

    Reverted all changes back to version 398092366484f0b3d20af47a8ca664c3fb596aee

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"; // Importa il componente Analytics
 
 createRoot(document.getElementById("root")!).render(<App />);
