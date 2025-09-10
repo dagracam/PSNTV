6a2ed598958a4185e036a3b87409ac3441b76deb
[dyad] <dagracaman@gmail.com>
2025-09-10 23:58:47 +0200

    Updated src/pages/TuttoRugby.tsx

--- src/pages/TuttoRugby.tsx
+++ src/pages/TuttoRugby.tsx
@@ -3,6 +3,9 @@ import Layout from '@/components/Layout';
 
 const TuttoRugby = () => {
   return (
+     </div>
+        <p className="mt-8 text-lg text-dyad-text/80">
+          Qui trovi tutti gli approfondimenti, le interviste e le analisi sul mondo del rugby.
     <Layout>
       <div className="container mx-auto p-4">
         <h1 className="text-4xl font-bold mb-8 text-dyad-text">Tutto Rugby</h1>
@@ -16,9 +19,7 @@ const TuttoRugby = () => {
             allowFullScreen
             className="w-full h-full border-none"
           ></iframe>
-        </div>
-        <p className="mt-8 text-lg text-dyad-text/80">
-          Qui trovi tutti gli approfondimenti, le interviste e le analisi sul mondo del rugby.
+      
         </p>
       </div>
     </Layout>
