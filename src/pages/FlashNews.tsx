504122b09e8a6bd4be02eede577ea4a7ffe88e47
[dyad] <dagracaman@gmail.com>
2025-10-14 18:09:50 +0200

    Updated src/pages/FlashNews.tsx

--- src/pages/FlashNews.tsx
+++ src/pages/FlashNews.tsx
@@ -28,6 +28,15 @@ const FlashNews: React.FC = () => {
 
         {/* L'iframe è stato rimosso come richiesto */}
         <div className="aspect-video w-full bg-black rounded-lg overflow-hidden shadow-xl flex items-center justify-center">
+        <iframe
+            width="100%"
+            height="545"
+            src={'https://web.psntv.eu/embed-playlist/persemprenews/LaVitaquestoPalcoscenico'}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="w-full h-full border-0"
+          ></iframe>
           <p className="text-dyad-text/70 text-xl">Contenuto video non disponibile.</p>
         </div>
       </div>
