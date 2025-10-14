9c6a198af56399827a183f1595622c05916e9cb4
[dyad] <dagracaman@gmail.com>
2025-10-14 20:59:40 +0200

    [dyad] Aggiunte le rotte mancanti per le pagine dei programmi. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,23 @@ import ParlamiDAmore from './pages/ParlamiDAmore';
 import LaSaluteInUnClick from './pages/LaSaluteInUnClick';
 import LaVitaQuestoPalcoscenico from './pages/LaVitaQuestoPalcoscenico';
 import InSicurezza from './pages/InSicurezza';
+import PerSempreConDiego from './pages/PerSempreConDiego'; // Import aggiunto
+import PerSempreScugnizzo from './pages/PerSempreScugnizzo'; // Import aggiunto
+import DaysOfWar from './pages/DaysOfWar'; // Import aggiunto
+import PsnSportClub from './pages/PsnSportClub'; // Import aggiunto
+import TuttoRugby from './pages/TuttoRugby'; // Import aggiunto
+import UrbanTalk from './pages/UrbanTalk'; // Import aggiunto
+import Schole from './pages/Schole'; // Import aggiunto
+import AboutUsPage from './pages/AboutUsPage'; // Import aggiunto per la pagina "Chi siamo"
+import PlaceholderPage from './pages/PlaceholderPage'; // Import aggiunto per le pagine placeholder
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />} /> {/* Questa è la vecchia pagina About */}
+        <Route path="/about-us" element={<AboutUsPage />} /> {/* Nuova pagina Chi siamo */}
         <Route path="/contact" element={<Contact />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-of-service" element={<TermsOfService />} />
@@ -27,6 +37,15 @@ function App() {
         <Route path="/lasaluteinunclick" element={<LaSaluteInUnClick />} />
         <Route path="/lavitaquestopalcoscenico" element={<LaVitaQuestoPalcoscenico />} />
         <Route path="/insicurezza" element={<InSicurezza />} />
+        <Route path="/persemprecondiego" element={<PerSempreConDiego />} /> {/* Rotta aggiunta */}
+        <Route path="/persempre-scugnizzo" element={<PerSempreScugnizzo />} /> {/* Rotta aggiunta */}
+        <Route path="/daysofwar" element={<DaysOfWar />} /> {/* Rotta aggiunta */}
+        <Route path="/psnsportclub" element={<PsnSportClub />} /> {/* Rotta aggiunta */}
+        <Route path="/tuttorugby" element={<TuttoRugby />} /> {/* Rotta aggiunta */}
+        <Route path="/urbantalk" element={<UrbanTalk />} /> {/* Rotta aggiunta */}
+        <Route path="/schole" element={<Schole />} /> {/* Rotta aggiunta */}
+        {/* Rotte per le pagine placeholder dal MoreMenuDialog */}
+        <Route path="/cookie-preferences" element={<PlaceholderPage title="Preferenze dei cookie" />} />
       </Routes>
     </Router>
   );
