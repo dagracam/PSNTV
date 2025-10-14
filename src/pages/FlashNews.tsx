64c143773b601e8e876fdd4f4d2317c18e770e20
[dyad] <dagracaman@gmail.com>
2025-10-14 18:03:24 +0200

    [dyad] Rimosso l'iframe dalla pagina Flash News. - wrote 1 file(s)

--- src/pages/FlashNews.tsx
+++ src/pages/FlashNews.tsx
@@ -26,16 +26,9 @@ const FlashNews: React.FC = () => {
           {program.description}
         </p>
 
-        <div className="aspect-video w-full bg-black rounded-lg overflow-hidden shadow-xl">
-         <iframe
-            width="100%"
-            height="545"
-            src={program.videoUrl} // Usa l'URL del video specifico del programma dai dati
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className="w-full h-full border-0"
-          ></iframe>
+        {/* L'iframe è stato rimosso come richiesto */}
+        <div className="aspect-video w-full bg-black rounded-lg overflow-hidden shadow-xl flex items-center justify-center">
+          <p className="text-dyad-text/70 text-xl">Contenuto video non disponibile.</p>
         </div>
       </div>
     </Layout>
