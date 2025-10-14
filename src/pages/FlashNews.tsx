5c5bd7ddc10ea0256f889be1eb6921f70ceaa6a8
[dyad] <dagracaman@gmail.com>
2025-10-14 18:17:11 +0200

    [dyad] Aggiornata la pagina Flash News per mostrare solo titolo e descrizione. - wrote 1 file(s)

--- src/pages/FlashNews.tsx
+++ src/pages/FlashNews.tsx
@@ -25,20 +25,6 @@ const FlashNews: React.FC = () => {
         <p className="text-lg text-dyad-text/80">
           {program.description}
         </p>
-
-        {/* L'iframe è stato rimosso come richiesto */}
-        <div className="aspect-video w-full bg-black rounded-lg overflow-hidden shadow-xl flex items-center justify-center">
-        <iframe
-            width="100%"
-            height="545"
-            src={'https://web.psntv.eu/embed-playlist/persemprenews/LaVitaquestoPalcoscenico'}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className="w-full h-full border-0"
-          ></iframe>
-          <p className="text-dyad-text/70 text-xl">Contenuto video non disponibile.</p>
-        </div>
       </div>
     </Layout>
   );
