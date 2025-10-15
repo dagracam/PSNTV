54520f578930a792d67af16b6bc8b3857a54c523
[dyad] <dagracaman@gmail.com>
2025-10-15 22:48:22 +0200

    [dyad] Aggiunto il codice di iubenda per la Privacy Policy. - wrote 2 file(s)

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -6,7 +6,9 @@ const PrivacyPolicy: React.FC = () => {
     <Layout>
       <div className="max-w-4xl mx-auto text-center py-10">
         <h1 className="text-4xl font-bold text-dyad-text mb-4">Informativa sulla Privacy</h1>
-        <p className="text-lg text-dyad-text/80">
+        {/* Codice iubenda aggiunto qui */}
+        <a href="https://www.iubenda.com/privacy-policy/54881883" className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">Privacy Policy</a>
+        <p className="text-lg text-dyad-text/80 mt-4">
           Questa è la nostra Informativa sulla Privacy. Qui spieghiamo come raccogliamo, usiamo e proteggiamo i tuoi dati.
         </p>
       </div>
